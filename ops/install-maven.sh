0646ff79af7a42a5cdbfafe3b389c5daaf86bbad
codecason <kindom07@163.com>
2021-07-25 23:52:41 +0800

    update install maven

--- ops/install-maven.sh
+++ ops/install-maven.sh
@@ -1,1 +1,17 @@
 wget https://apache.website-solution.net/maven/maven-3/3.8.1/binaries/apache-maven-3.8.1-bin.zip
+
+
+# 换源加速
+阿里云的：
+http://maven.aliyun.com/nexus/content/groups/public/
+
+配置方法：在maven安装目录的setting.xml中配置
+
+<mirrors>
+	<mirror>
+	  <id>alimaven</id>
+	  <name>aliyun maven</name>
+	  <url>http://maven.aliyun.com/nexus/content/groups/public/</url>
+	  <mirrorOf>central</mirrorOf>        
+	</mirror>
+</mirrors>
