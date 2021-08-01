0f3612e8dea14c180b6fa2064f4d1768ebb11a55
codecason <kindom07@163.com>
2021-08-01 11:49:32 +0800

    update java concurrency

---
+++ spring+java/ConcurrencyDemo2.java
@@ -0,0 +1,25 @@
+// https://stackoverflow.com/questions/17748078/simplest-and-understandable-example-of-volatile-keyword-in-java
+// 构造一个没有volatile则无法停止的样例
+class ConcurrencyDemo2 extends Thread {
+    //volatile
+    boolean keepRunning = true;
+
+    public void run() {
+        long count=0;
+        while (keepRunning) {
+            count++;
+        }
+
+        System.out.println("Thread terminated." + count);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ConcurrencyDemo2 t = new ConcurrencyDemo2();
+        t.start();
+        Thread.sleep(1000);
+        System.out.println("after sleeping in main");
+        t.keepRunning = false;
+        t.join();
+        System.out.println("keepRunning set to " + t.keepRunning);
+    }
+}
